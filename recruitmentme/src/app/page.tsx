c1453dfffbb51fc1c7369bab74b39a5a515f1435
xleonard1 <xavier.lnrd@gmail.com>
2024-07-30 22:36:14 -0400

    begin setting up localization

--- recruitmentme/src/app/page.tsx
+++ recruitmentme/src/app/page.tsx
@@ -1,16 +1,16 @@
-import Image from "next/image";
-import styles from "./page.module.css";
-import {Box, Stack} from "@mui/material"
+import {Box, Stack, Typography} from "@mui/material"
 
 
 export default function Home() {
+  
   return (
-   
       <Box height="100vh" sx={{backgroundColor: "#F0FFFF"}}>
         <Box p={5}>
           <Stack direction={"row"} justifyContent={"space-between"}>
             <Box  height="100vh" width="100%" sx={{backgroundColor: "#F0FFFF"}}>
-          
+              <Typography  variant="h1">
+                RealHire
+              </Typography>
            </Box>
             <Box  
               ml={5}
